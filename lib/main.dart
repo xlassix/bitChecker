659d9d830bd3ea92964522b792b8fa3554704c42
xlassix <awhy14539@gmail.com>
2021-01-02 23:43:32 +0100

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:bit_tracker/screens/home.dart';
+import 'package:bit_tracker/screens/loading.dart';
 import 'package:bit_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomeScreen(),
+    home: Loading(),
     theme: ThemeData.dark().copyWith(
       appBarTheme: AppBarTheme(
         color: kThemePriColor,
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,46 @@
 import 'package:bit_tracker/components/currencySelector.dart';
 import 'package:bit_tracker/components/priceContainer.dart';
 import 'package:bit_tracker/utils/coinData.dart';
+import 'package:bit_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  final Map data;
+  HomeScreen({@required this.data});
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   String _currencyName = "USD";
+  Map data;
+  Map exchangeData;
 
-  void updateCurrency(String value) {
+  CoinData instance = CoinData(coinNames: kCryptoList);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      exchangeData = widget.data;
+    });
+    updateCurrency(_currencyName);
+  }
+
+  void updateCurrency(String value) async {
+    var temp = await instance.getCurrentPrices();
+    print(exchangeData[value]);
     setState(() {
       _currencyName = value;
+      data = temp;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    CoinData instance = CoinData(coinNames: ["67", "ETH", "btc"]);
-    instance.getCurrentPrices();
+    data = data ??
+        Map.fromIterables(kCryptoList, List.filled(kCryptoList.length, "?"));
     return Scaffold(
       appBar: AppBar(
         title: Text("BitTracker"),
@@ -28,10 +48,15 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          PriceContainer(
-            unit: "BTC",
-            value: "23424",
-            metric: "USD",
+          Column(
+            children: data.entries.map((element) {
+              print(element);
+              return PriceContainer(
+                unit: element.key,
+                value: element.value,
+                metric: "USD",
+              );
+            }).toList(),
           ),
           CurrencySelector(
             currentValue: _currencyName,
