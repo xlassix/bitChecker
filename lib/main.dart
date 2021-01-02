99edb1cfefe8934906dfb7289293bf1073903c7f
xlassix <awhy14539@gmail.com>
2021-01-02 13:53:23 +0100

    dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:bit_tracker/screens/home.dart';
+import 'package:bit_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: HomeScreen(),
+    theme: ThemeData.dark().copyWith(
+      appBarTheme: AppBarTheme(
+        color: kThemePriColor,
+        centerTitle: true,
+      ),
+      scaffoldBackgroundColor: kScaffoldBackground,
+      primaryColor: kPrimaryColor,
+    ),
+  ));
 }
-
